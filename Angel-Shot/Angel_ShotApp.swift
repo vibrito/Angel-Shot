4941a055e09e269bd8eeb176feb24b385577288f
Vinicius Brito <vibrito@gmail.com>
2023-05-19 00:25:50 -0300

    First Commit

---
+++ Angel-Shot/Angel_ShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Angel_ShotApp.swift
+//  Angel-Shot
+//
+//  Created by Vinicius Brito on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Angel_ShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Angel-ShotUITests/Angel_ShotUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Angel_ShotUITestsLaunchTests.swift
+//  Angel-ShotUITests
+//
+//  Created by Vinicius Brito on 5/19/23.
+//
+
+import XCTest
+
+final class Angel_ShotUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
